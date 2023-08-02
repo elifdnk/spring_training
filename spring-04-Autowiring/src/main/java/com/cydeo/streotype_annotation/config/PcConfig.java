5df56498de0ea4b92dc2807b4133146db9522584
ELİF DENK <elifdenkk@gmail.com>
2023-08-02 10:54:29 +0300

    Autowiring

---
+++ spring-04-Autowiring/src/main/java/com/cydeo/streotype_annotation/config/PcConfig.java
@@ -0,0 +1,19 @@
+package com.cydeo.streotype_annotation.config;
+
+
+import com.cydeo.streotype_annotation.casefactory.Case;
+import com.cydeo.streotype_annotation.casefactory.DellCase;
+import com.cydeo.streotype_annotation.casefactory.Dimensions;
+import lombok.Getter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.cydeo.streotype_annotation")
+public class PcConfig {
+
+
+
+
+}

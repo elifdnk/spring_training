54da932f8907f5a3ac4226e20885137d999a235c
ELİF DENK <elifdenkk@gmail.com>
2023-08-11 16:42:23 +0300

    MVC Controller

---
+++ spring-08-MVC-Controller/src/main/java/com/cydeo/MvcControllerApplication.java
@@ -0,0 +1,14 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.aop.AopAutoConfiguration;
+@SpringBootApplication
+//@SpringBootApplication(exclude = {AopAutoConfiguration.class})
+public class MvcControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcControllerApplication.class, args);
+	}
+
+}

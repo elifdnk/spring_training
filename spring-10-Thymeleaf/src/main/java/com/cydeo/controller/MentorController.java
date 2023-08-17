1f3f385d5655c01afe24a05abf752b70cc764a20
ELİF DENK <elifdenkk@gmail.com>
2023-08-18 01:32:00 +0300

    Creating Form

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -1,4 +1,20 @@
 package com.cydeo.controller;
 
+import com.cydeo.model.Mentor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mentor")
 public class MentorController {
+
+        @GetMapping("/register")  //localhost:8080/mentor/register
+        public String register(Model model){
+
+            model.addAttribute("mentor",new Mentor());
+
+            return "mentor/mentor-register";
+        }
 }
---
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/model/Mentor.java
@@ -0,0 +1,21 @@
+package com.cydeo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Mentor {
+
+    private String firstName,
+            lastName,
+            email,
+            gender,
+            batch,
+            company;
+    private boolean graduated;
+
+
+}

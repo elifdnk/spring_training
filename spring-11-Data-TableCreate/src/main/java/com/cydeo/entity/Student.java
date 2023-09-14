b21e7de3d21f789f25a7ab92441c0e9a6b5e6ade
ELİF DENK <elifdenkk@gmail.com>
2023-09-15 00:56:11 +0300

    @Column added

--- spring-11-Data-TableCreate/src/main/java/com/cydeo/entity/Student.java
+++ spring-11-Data-TableCreate/src/main/java/com/cydeo/entity/Student.java
@@ -1,15 +1,17 @@
 package com.cydeo.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Student {
-    
-@Id
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "studentFirstName")
     private String firstName;
+    @Column(name = "studentLastName")
     private String lastName;
     private String email;
 }

063a6019bf533dbf56667b2d7bf0112c4081bfa9
ELİF DENK <elifdenkk@gmail.com>
2023-09-15 23:10:58 +0300

    CreditAccount and Debit  class created

---
+++ spring-11-Data-TableCreate/src/main/java/com/cydeo/entity/CreditAccount.java
@@ -0,0 +1,8 @@
+package com.cydeo.entity;
+
+import java.math.BigDecimal;
+
+public class CreditAccount {
+
+    private BigDecimal creditLimit;
+}
---
+++ spring-11-Data-TableCreate/src/main/java/com/cydeo/entity/DebitAccount.java
@@ -0,0 +1,8 @@
+package com.cydeo.entity;
+
+import java.math.BigDecimal;
+
+public class DebitAccount {
+
+    private BigDecimal overDraftFee;
+}

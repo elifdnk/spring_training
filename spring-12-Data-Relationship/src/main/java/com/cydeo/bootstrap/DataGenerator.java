e0b7ac2e10f6afe436a246929b7e976ceef74ee7
ELİF DENK <elifdenkk@gmail.com>
2023-09-18 13:06:14 +0300

    cascade type is changing

--- spring-12-Data-Relationship/src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ spring-12-Data-Relationship/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -33,5 +33,12 @@ public class DataGenerator implements CommandLineRunner {
 
         paymentRepository.save(payment1);
         paymentRepository.save(payment2);
+
+        System.out.println(paymentRepository.findById(2L).get().getPaymentDetail().getCommissionAmount());
+
+        paymentRepository.delete(payment1);
+
+
+
     }
 }

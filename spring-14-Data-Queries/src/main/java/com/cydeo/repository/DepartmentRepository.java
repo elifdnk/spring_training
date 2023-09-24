a4397262606b0fb334a27e0460b312263c787ced
ELİF DENK <elifdenkk@gmail.com>
2023-09-24 22:51:39 +0300

    Department Repository is created

---
+++ spring-14-Data-Queries/src/main/java/com/cydeo/repository/DepartmentRepository.java
@@ -0,0 +1,15 @@
+package com.cydeo.repository;
+
+import com.cydeo.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface DepartmentRepository extends JpaRepository<Department,String> {
+
+    //Display all departments in the furniture department
+    List<Department> findByDepartment(String department);
+
+    //
+
+}

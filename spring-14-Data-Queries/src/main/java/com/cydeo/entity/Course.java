5e1995c9766fd2eab915024963ae32ef042806ba
ELİF DENK <elifdenkk@gmail.com>
2023-09-25 21:59:16 +0300

    Course class added

---
+++ spring-14-Data-Queries/src/main/java/com/cydeo/entity/Course.java
@@ -0,0 +1,29 @@
+package com.cydeo.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@NoArgsConstructor
+@Data
+@Table(name = "COURSES")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "NAME")
+    private String name;
+    @Column(name = "CATEGORY")
+    private String category;
+    @Column(name = "RATING")
+    private int Rating;
+    @Column(name = "DESCRIPTION")
+    private String description;
+
+
+
+}

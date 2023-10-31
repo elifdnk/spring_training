bd4ddb6b8b0ca3a2fa5d525366efbc32c74ac3ab
ELİF DENK <elifdenkk@gmail.com>
2023-10-31 15:23:15 +0300

    @JsonIgnore annotation comment added

--- spring-15-REST/src/main/java/com/cydeo/dto/CourseDTO.java
+++ spring-15-REST/src/main/java/com/cydeo/dto/CourseDTO.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CourseDTO {
 
-    @JsonIgnore
+    @JsonIgnore //in the JSON we will not see id field
     private Long id;
     private String name;
     private String category;
